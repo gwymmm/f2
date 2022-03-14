a4beeeb2db605e686c0f10a737800fbdd2d26aeb
gwymmm <dn8733@yahoo.com>
2022-03-14 21:31:16 +0100

    removed ws

--- source/std_string_handling.adb
+++ source/std_string_handling.adb
@@ -1,28 +1,28 @@
 package body STD_String_Handling is
-  
+
   procedure Set(Target: out STD_String; Source: in String) is
   begin
     Target := To_Unbounded_String(Source);
   end Set;
-  
+
   function Get(Source: in STD_String) return String is
   begin
     return To_String(Source);
   end Get;
-  
+
   procedure Set_Character(Target: in out STD_String; Position: in Positive; Char: in Character) is
   begin
     Replace_Element(Target, Position, Char);
   end Set_Character;
-  
+
   function Get_Character(From_String: in STD_String; Position: in Positive) return Character is
   begin
     return Element(From_String, Position);
   end Get_Character;
-  
+
   procedure Append_Character(Target: in out STD_String; Char: in Character) is
   begin
     Append(Target, Char);
   end Append_Character;
-      
+
 end STD_String_Handling;
--- source/std_string_handling.ads
+++ source/std_string_handling.ads
@@ -1,21 +1,21 @@
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 package STD_String_Handling is
-  
+
   type STD_String is private;
-  
+
   procedure Set(Target: out STD_String; Source: in String);
-  
+
   function Get(Source: in STD_String) return String;
-  
+
   procedure Set_Character(Target: in out STD_String; Position: in Positive; Char: in Character);
-  
+
   function Get_Character(From_String: in STD_String; Position: in Positive) return Character;
-  
+
   procedure Append_Character(Target: in out STD_String; Char: in Character);
-  
+
 private
-  
+
   type STD_String is new Unbounded_String;
-  
+
 end STD_String_Handling;
